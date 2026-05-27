f9d67f58211cd674750046872ace449fd680faad
vishnu <vishnuvardhanyaragalla5@gmail.com>
2026-05-27 11:15:43 +0530

    Updated database config

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "postgresql://postgres:root@localhost/fake_job_detector"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
