caabc654b1a22582f33349d5a2aae88e00780d40
vishnu <vishnuvardhanyaragalla5@gmail.com>
2026-05-27 13:03:30 +0530

    Updated risk score and confidence

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,9 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "postgresql://postgres:root@localhost/fake_job_detector"
 
 engine = create_engine(DATABASE_URL)
 
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,3 @@
-from app.routes.url_checker import router as url_router
 from app.routes.dashboard import router as dashboard_router
 from app.database import engine
 from app.models import Base
@@ -8,6 +7,7 @@ from app.routes.detect import router
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -18,9 +18,7 @@ app.add_middleware(
 
 app.include_router(router)
 app.include_router(dashboard_router)
-app.include_router(url_router)
 
 @app.get("/")
 def home():
     return {"message": "Fake Job Detector API Running"}
-
