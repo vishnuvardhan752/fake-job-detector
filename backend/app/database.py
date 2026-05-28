2c5c1996ff03920907aa547a0b102d3847f12273
vishnu <vishnuvardhanyaragalla5@gmail.com>
2026-05-28 10:30:33 +0530

    Fixed database connection for Render

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,10 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:root@localhost/fake_job_detector"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
